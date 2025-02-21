5b9afc14b35605a9aae5d6ee8f79adf3d00e9c59
luisjg04 <luisjg04@ucm.es>
2025-02-21 14:17:34 +0100

    Cambiada interfaz a clase

--- src/logic/seleccion/Seleccionable.java
+++ src/logic/seleccion/Seleccionable.java
@@ -1,12 +1,47 @@
 package logic.seleccion;
 
-public interface Seleccionable {
-    double getFitness();
-    double getProb();
-    double getAccProb();
+public class Seleccionable {
+    public int ind;
+    public double fitness;
+    public double prob;
+    public double accProb;
 
-    void setFitness(double fitness);
-    void setProb(double prob);
-    void setAccProb(double accProb);
+    public Seleccionable(int ind, double fitness, double prob, double accProb) {
+        this.ind = ind;
+        this.fitness = fitness;
+        this.prob = prob;
+        this.accProb = accProb;
+    }
 
+    public int getInd() {
+        return ind;
+    }
+
+    public void setInd(int ind) {
+        this.ind = ind;
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public double getProb() {
+        return prob;
+    }
+
+    public void setProb(double prob) {
+        this.prob = prob;
+    }
+
+    public double getAccProb() {
+        return accProb;
+    }
+
+    public void setAccProb(double accProb) {
+        this.accProb = accProb;
+    }
 }
