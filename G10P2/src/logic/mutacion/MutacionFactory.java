96855c4d3c327139ae3f8df70fa97ce00ebc083a
luisjg04 <luisjg04@ucm.es>
2025-03-20 22:05:45 +0100

    MutacionInvencion implementado

--- G10P2/src/logic/mutacion/MutacionFactory.java
+++ G10P2/src/logic/mutacion/MutacionFactory.java
@@ -11,6 +11,7 @@ public class MutacionFactory {
         mutations.put(1,new MutacionIntercambio());
         mutations.put(2,new MutacionInversion());
         mutations.put(3,new MutacionHeuristica());
+        mutations.put(4,new MutacionInversion());
     }
 
     public static Mutate getMutation(int index){
