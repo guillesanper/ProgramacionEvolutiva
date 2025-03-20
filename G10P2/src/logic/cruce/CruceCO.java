a6f58da5ae8f2cbde74d96e97dc323a66ff078a0
luisjg04 <luisjg04@ucm.es>
2025-03-20 22:06:22 +0100

    Cruce ordinal arreglado

--- G10P2/src/logic/cruce/CruceCO.java
+++ G10P2/src/logic/cruce/CruceCO.java
@@ -1,6 +1,7 @@
 package logic.cruce;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -15,38 +16,23 @@ public class CruceCO extends Cruce<Integer> {
         Random rand = new Random();
         this.tamCromosoma = c1.length;
 
-
         // Codificamos las permutaciones a representación ordinal
-
         Integer[] p1cod = codificarOrdinal(c1);
         Integer[] p2cod = codificarOrdinal(c2);
-
-
-
         // Generamos un punto de cruce aleatorio (cruce de un punto)
-        int puntoCruce = rand.nextInt(tamCromosoma - 1) + 1;
+        int cut = rand.nextInt(tamCromosoma-1)+1;
 
         // Realizamos el cruce de permutaciones
-        Integer[] hijoPermutacion1 = new Integer[tamCromosoma];
-        Integer[] hijoPermutacion2 = new Integer[tamCromosoma];
-
-        // Copiamos la primera parte de cada padre
-        for (int i = 0; i < puntoCruce; i++) {
-            hijoPermutacion1[i] = p1cod[i];
-            hijoPermutacion2[i] = p2cod[i];
-        }
-
-        // Completamos los hijos con los elementos restantes en el orden del otro padre
-        completarHijoPermutacion(p2cod, hijoPermutacion1, puntoCruce);
-        completarHijoPermutacion(p1cod, hijoPermutacion2, puntoCruce);
+        Integer[] hijoCod1 = crearHijo(p1cod, p2cod, cut);
+        Integer[] hijoCod2 = crearHijo(p2cod, p1cod, cut);
 
         // Decodificamos los cromosomas ordinales a permutaciones de habitaciones
-        Integer[] hijo1 = decodificarOrdinal(hijoPermutacion1);
-        Integer[] hijo2 = decodificarOrdinal(hijoPermutacion2);
+        Integer[] hijo1 = decodificarOrdinal(hijoCod1);
+        Integer[] hijo2 = decodificarOrdinal(hijoCod2);
 
         // Copiamos los hijos a los padres
-        System.arraycopy(p1cod, 0, c1, 0, tamCromosoma);
-        System.arraycopy(p2cod, 0, c2, 0, tamCromosoma);
+        System.arraycopy(hijo1, 0, c1, 0, tamCromosoma);
+        System.arraycopy(hijo2, 0, c2, 0, tamCromosoma);
     }
 
     /**
@@ -107,44 +93,17 @@ public class CruceCO extends Cruce<Integer> {
         return codificacion;
     }
 
-    /**
-     * Completa un hijo añadiendo los elementos restantes en el orden en que aparecen en el otro padre,
-     * evitando duplicados.
-     * @param padreOrigen El padre que proporciona el orden.
-     * @param hijo El hijo que se está completando.
-     * @param puntoCruce El punto de cruce.
-     */
-    private void completarHijoPermutacion(Integer[] padreOrigen, Integer[] hijo, int puntoCruce) {
-        // Array para marcar los elementos ya incluidos en el hijo.
-        // Asumimos que los valores válidos están entre 0 y tamCromosoma-1.
-        boolean[] elementosIncluidos = new boolean[tamCromosoma];
+    private Integer[] crearHijo(Integer[] c1, Integer[] c2, int cut) {
+        // Realizamos el cruce de permutaciones
+        Integer[] hijo = new Integer[tamCromosoma];
 
-        // Marcamos los elementos ya copiados en el hijo.
-        for (int i = 0; i < puntoCruce; i++) {
-            elementosIncluidos[hijo[i]] = true;
-        }
+        Integer[] firstPart = Arrays.copyOfRange(c1, 0, cut-1);
+        Integer[] secondPart = Arrays.copyOfRange(c2, cut-1, c2.length);
 
-        int indiceHijo = puntoCruce;
-        // Recorremos el padre origen e incorporamos los elementos que aún no se hayan incluido.
-        for (int i = 0; i < tamCromosoma && indiceHijo < tamCromosoma; i++) {
-            int valor = padreOrigen[i];
-            if (!elementosIncluidos[valor]) {
-                hijo[indiceHijo] = valor;
-                elementosIncluidos[valor] = true;
-                indiceHijo++;
-            }
-        }
+        System.arraycopy(firstPart, 0, hijo, 0, firstPart.length);
+        System.arraycopy(secondPart, 0, hijo, firstPart.length, secondPart.length);
 
-        // Si por alguna razón aún faltan elementos (lo cual no debería ocurrir en una permutación válida),
-        // los completamos buscando los valores que no se han incluido.
-        if (indiceHijo < tamCromosoma) {
-            for (int valor = 0; valor < tamCromosoma && indiceHijo < tamCromosoma; valor++) {
-                if (!elementosIncluidos[valor]) {
-                    hijo[indiceHijo] = valor;
-                    indiceHijo++;
-                }
-            }
-        }
+        return hijo;
     }
 
 }
