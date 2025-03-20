75610811136ec369d07a74e6ae8f86e0a5eb7094
luisjg04 <luisjg04@ucm.es>
2025-03-20 15:49:22 +0100

    MutacionInvencion implementado

---
+++ G10P2/src/logic/mutacion/MutacionInvencion.java
@@ -0,0 +1,22 @@
+package logic.mutacion;
+
+import java.util.Arrays;
+
+public class MutacionInvencion implements Mutate {
+    @Override
+    public Integer[] mutate(Integer[] chromosome) {
+        Integer[] mutation = new Integer[chromosome.length];
+        // 1 2 3 4 | 5 6 7 8 9
+        // 0 1 2 3 | 4 5 6 7 8
+        int cut = chromosome.length/2;
+
+        Integer[] firstHalf = Arrays.copyOfRange(chromosome, 0, cut);
+        Integer[] secondHalf = Arrays.copyOfRange(chromosome, cut, chromosome.length);
+
+        System.arraycopy(secondHalf, 0, mutation, 0, secondHalf.length);
+        System.arraycopy(firstHalf, 0, mutation, secondHalf.length, firstHalf.length);
+
+
+        return mutation;
+    }
+}
