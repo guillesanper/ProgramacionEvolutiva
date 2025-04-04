56036294d43304aef9f15f95dbf24317dc0b2364
luisjg04 <luisjg04@ucm.es>
2025-04-04 15:34:03 +0200

    TerminalMutation terminada

--- G10P3/src/logic/mutacion/FunctionalMutation.java
+++ G10P3/src/logic/mutacion/FunctionalMutation.java
@@ -1,4 +1,21 @@
 package logic.mutacion;
 
-public class FunctionalMutation {
+import model.Tree;
+import model.symbol.Expression;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FunctionalMutation implements Mutacion {
+    @Override
+    public Tree mutate(Tree t) {
+        List<Expression> functions = new ArrayList<>();
+        t.collectFunctions(functions);
+
+        int choice = new Random().nextInt(functions.size());
+        functions.get(choice).mutate();
+
+        return t;
+    }
 }
--- G10P3/src/model/symbol/functions/IfFood.java
+++ G10P3/src/model/symbol/functions/IfFood.java
@@ -3,10 +3,12 @@ package model.symbol.functions;
 import model.symbol.Expression;
 
 public class IfFood extends Expression {
+    public static final String OP = "SICOMIDA";
+
     public IfFood() {
         super();
         this.childrenCount = 2;
-        this.operation = "SICOMIDA";
+        this.operation = IfFood.OP;
     }
 
     public IfFood(Expression a, Expression b) {
@@ -20,5 +22,10 @@ public class IfFood extends Expression {
     public Object execute(boolean isThereFood) {
         int child = isThereFood ? 0 : 1;
         return this.children.get(child).execute(isThereFood);
+    }
+
+    @Override
+    public void mutate() {
+        this.operation = Prog2.OP;
     }
 }
